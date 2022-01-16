a59c97f88e38bab26c169324cc5b9954b95e11f8
Jorge Andres Salgado Echeverria <39527509+jsalgadoe@users.noreply.github.com>
2022-01-16 18:47:13 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   firebaseConfig: {
-    projectId: 'graficos-firebase',
-    appId: '1:820742273615:web:7135d69c76dd953f76083a',
-    storageBucket: 'graficos-firebase.appspot.com',
-    locationId: 'us-central',
-    apiKey: 'AIzaSyDD9MxPYFWnIRnat3FPDcGtGfvnzIzMHPE',
-    authDomain: 'graficos-firebase.firebaseapp.com',
-    messagingSenderId: '820742273615',
+    projectId: '',
+    appId: '',
+    storageBucket: '',
+    locationId: '',
+    apiKey: '',
+    authDomain: '',
+    messagingSenderId: '',
   },
   production: true,
   url: 'https://us-central1-graficos-firebase.cloudfunctions.net'
